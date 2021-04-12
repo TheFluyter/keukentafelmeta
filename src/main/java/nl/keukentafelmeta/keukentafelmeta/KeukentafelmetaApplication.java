445a986e1b77210e5f0051a7377ef7cd7c2682ad
TheFluyter <rick__ruiter@hotmail.com>
2021-04-12 20:46:47 +0200

    Initial commit

---
+++ src/main/java/nl/keukentafelmeta/keukentafelmeta/KeukentafelmetaApplication.java
@@ -0,0 +1,13 @@
+package nl.keukentafelmeta.keukentafelmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeukentafelmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeukentafelmetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/keukentafelmeta/keukentafelmeta/KeukentafelmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.keukentafelmeta.keukentafelmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeukentafelmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
